fafac243eb5fa1bbbcb46699b1d97d8901f4c3c8
rafaesky <erikarafaeskyti@gmail.com>
2024-11-26 16:04:06 -0300

    Simple Database and Routes

--- src/server.js
+++ src/server.js
@@ -1,26 +1,18 @@
 import http from 'http';
 import { json } from './middlewares/json.js';
+import { routes } from './routes.js';
 const port = process.env.PORT || 3000
 
-const users = [];
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
   await json(req, res)
-
-  if (method === 'GET' && url === '/users') {
-    return res.end(JSON.stringify(users))
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
+  if (route) {
+    return route.handler(req, res)
   }
 
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    users.push({
-      id: 1,
-      name: name,
-      email: email
-    })
-    return res.writeHead(201).end('Create user')
-  }
   return res.writeHead(404).end('Not find')
 });
 
