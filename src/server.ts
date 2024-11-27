66715a4972fadbd7db01242e7a6640d784b24277
rafaesky <erikarafaeskyti@gmail.com>
2024-11-27 10:16:15 -0300

    New Node API with Typescript

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get('/hello', () => {
+  return 'Hello Word'
+})
+
+app.listen({ port: 3333 }).then(() => { console.log('Http console server running') })
--- src/utils/buid-routh-path.js
+++
@@ -1,9 +0,0 @@
-
-export function buidRouterPath(path) {
-  const routeParametersRegex = /:([a-zA-zZ]+)/g
-  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<id>[a-z0-9\-_]+)');
-
-  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
-
-  return pathRegex
-}
