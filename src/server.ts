982f71dca5ebe399f6e4f71198ffb1fcca0155bc
rafaesky <erikarafaeskyti@gmail.com>
2024-11-27 15:57:52 -0300

    Variáveis ambiente e tipagem com knex

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,10 @@
-import { randomUUID } from 'crypto'
 import fastify from 'fastify'
-import { knex } from './database'
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
+app.register(transactionsRoutes, { prefix: 'transactions' })
 
-app.post('/', async () => {
-  const transaction = await knex('transactions').insert({
-    id: randomUUID(),
-    title: 'Teste',
-    amount: 1000
-  })
-  return transaction
-})
-app.get('/', async () => {
-  const transaction = await knex('transactions').where('amount', 1000).select('*')
-  return transaction
-})
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('Http console server running')
 })
