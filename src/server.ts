e94b917e6e025962e732647e560bb9aec7529643
rafaesky <erikarafaeskyti@gmail.com>
2024-11-28 16:05:19 -0300

    Aliases de importação

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 import { app } from './app';
 import { env } from './env';
 
-
 app.listen({
-  host: '0.0.0.0',
-  port: env
-}).then(() => console.log('HTTP Server Running'))
+  port: env.PORT,
+  host: `0.0.0.0`
+}).then(() => { console.log('Http console server running') })
