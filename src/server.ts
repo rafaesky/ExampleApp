9174abeacc2a36edc25ff6c44eb0684a89efc9b2
rafaesky <erikarafaeskyti@gmail.com>
2024-11-28 14:33:54 -0300

    Ajustando variável da porta Host para o Render

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import { app } from "./app"
 import { env } from './env'
-app.listen({ port: env.PORT }).then(() => {
+app.listen({
+  port: env.PORT,
+  host: 'RENDER' in process.env ? `0.0.0.0` : `localhost`
+}).then(() => {
   console.log('Http console server running')
 })
