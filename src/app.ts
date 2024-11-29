ed1273fe29d3a5a1b928df7eba1ca9b1cf4389c9
rafaesky <erikarafaeskyti@gmail.com>
2024-11-29 11:54:33 -0300

    Configuração prisma e docker

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
-// import cookie from '@fastify/cookie'
+import { prisma } from '@/lib/prisma'
 import fastify from 'fastify'
-// import { transactionsRoutes } from './routes/transactions'
+import { z } from 'zod'
 
 export const app = fastify()
-app.addHook('preHandler', async (request, reply) => {
-  console.log("Hold on. Initiating the App")
+
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6)
+  })
+  const { name, email, password } = registerBodySchema.parse(request.body)
+  await prisma.user.create({
+    data: {
+      name, email, password_hash: password
+    }
+  })
+  return reply.status(201).send()
 })
-// app.register(cookie)
-// app.register(transactionsRoutes, { prefix: 'transactions' })
